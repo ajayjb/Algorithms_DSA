1399e55663407d81d066073d393aca802bd4a029
Ajay <ajayjb11@gmail.com>
2022-02-01 23:50:52 +0530

    Recursion level 1

---
+++ src/com/ajay/RecusionLevel1/ReverseANumber.java
@@ -0,0 +1,10 @@
+package com.ajay.RecusionLevel1;
+
+public class ReverseANumber {
+    public static void main(String[] args) {
+
+    }
+    static int rev(int num){
+        return 5;
+    }
+}
--- src/com/ajay/RecusionLevel1/SumDigits.java
+++ src/com/ajay/RecusionLevel1/SumDigits.java
@@ -1,4 +1,13 @@
 package com.ajay.RecusionLevel1;
 
 public class SumDigits {
+    public static void main(String[] args) {
+        System.out.println(find(1253));
+    }
+    static int find(int num){
+        if(num == 0){
+            return 0;
+        }
+        return num%10+find(num/10);
+    }
 }
